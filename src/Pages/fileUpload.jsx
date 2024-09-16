e52832cd5d9c2bf71119e8acbdf4d1ae4b3fc0ab
abhaydixit07 <abhaydixit227@gmail.com>
2024-09-17 00:27:26 +0530

    Render Thumnails Of Upload files

--- src/Pages/fileUpload.jsx
+++ src/Pages/fileUpload.jsx
@@ -34,47 +34,15 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-[#e0f7c5] flex items-center justify-center">
+    <div className="min-h-screen bg-gradient-to-r from-[#a8ff78] via-[#78ffd6] to-[#e0f7c5] flex items-center justify-center">
       <div className="bg-white shadow-md rounded-lg p-6 max-w-lg">
         <h1 className="text-2xl font-semibold mb-4">
           AyurGuru: Upload Your Report
         </h1>
         <form onSubmit={handleSubmit}>
-        {/* <div
-        className="w-[400px] relative border-2 border-gray-300 border-dashed rounded-lg p-6 m-10"
-        id="dropzone"
-        >
-        <input
-            type="file"
-            className="absolute inset-0 w-full h-full opacity-0 z-50"
-        />
-        <div className="text-center">
-            <img
-            className="mx-auto h-12 w-12"
-            src="https://www.svgrepo.com/show/357902/image-upload.svg"
-            alt=""
-            />
-            <h3 className="mt-2 text-sm font-medium text-gray-900">
-            <label htmlFor="file-upload" className="relative cursor-pointer">
-                <span>Drag and drop</span>
-                <span className="text-indigo-600"> or browse</span>
-                <span>to upload</span>
-                <input
-                id="file-upload"
-                name="file-upload"
-                type="file"
-                className="sr-only"
-                />
-            </label>
-            </h3>
-            <p className="mt-1 text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-        </div>
-        <img src="" className="mt-4 mx-auto max-h-40 hidden" id="preview" />
-        </div> */}
-        <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
-          <FileUpload onChange={handleFileUpload} />
-        </div>
-
+          <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
+            <FileUpload onChange={handleFileUpload} />
+          </div>
 
           <div className="mb-4">
             <label className="block text-sm font-medium mb-2">
